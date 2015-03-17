c55a58d738bad227f4a05c7880f603645c62956e
organicclockwork <rachaellw5.1@gmail.com>
2015-03-17 14:51:06 +0000

    Add a full title helper method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   
   test "home title is correct" do
     get :home
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
   
   #Pages Tests
