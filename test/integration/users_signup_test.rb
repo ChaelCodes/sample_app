750c8fffdf29b17dcf6e6907a58370facff73f20
organicclockwork <rachaellw5.1@gmail.com>
2015-05-12 00:10:35 +0000

    Use SSL and the puma webserver in production. (fix bug in int test case)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "invalid signup information" do
-    get signup_path
+    get new_user_path
     assert_no_difference 'User.count' do
       post users_path, user: { name:  "",
                                email: "user@invalid",
@@ -14,12 +14,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
   
   test "valid signup information" do
-    get signup_path
+    get new_user_path
     assert_difference 'User.count', 1 do
-      post_via_redirect user_path, user: { name: "Example User",
-                                           email: "user@example.com",
-                                           password: "password",
-                                           password_confirmation: "password" }
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
     end
     assert_template 'users/show'
   end
